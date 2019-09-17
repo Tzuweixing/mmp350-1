a91d65b43a9f1475431ed2644aa2a202f18314f6
Owen Roberts <oroberts@bmcc.cuny.edu>
2019-09-16 21:12:04 -0400

    testing login

--- responsive/js/auth.js
+++ responsive/js/auth.js
@@ -10,11 +10,10 @@ loginButton.onclick = function(event) {
 	});
 };
 
-/* auth state */
+
 const displayName = document.getElementById("user-name");
 
 firebase.auth().onAuthStateChanged(function(user) {
-	
 	if (user) {
 		document.body.classList.add('auth');
 		displayName.textContent = "Welcome, " + user.displayName;
@@ -29,14 +28,5 @@ const logoutButton = document.getElementById("logout-button");
 logoutButton.onclick = function() {
 	firebase.auth().signOut();
 };
-
-
-
-
-
-
-
-
-
 
 
