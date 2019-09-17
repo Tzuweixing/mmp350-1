5b6f5d66523e263984c7621bce4b90d59ee10f72
Owen Roberts <oroberts@bmcc.cuny.edu>
2019-09-16 21:12:04 -0400

    testing login

--- auth/auth.js
+++ auth/auth.js
@@ -3,8 +3,19 @@ const emailInput = document.getElementById("email");
 const passwordInput = document.getElementById("password");
 const message = document.getElementById("login-message");
 
-loginButton.onclick = function(event) {
-//	message.textContent = emailInput.value + " has logged in :)";
-	
-	firebase.auth().signInWithEmailAndPassword(emailInput.value, passwordInput.value);
+loginButton.onclick = function(event) {	
+	const promise = firebase.auth().signInWithEmailAndPassword(emailInput.value, passwordInput.value);
+	promise.catch(function(error) {
+		message.textContent = error.message;
+	});
 };
+
+firebase.auth().onAuthStateChanged(function(user) {
+	if (user) {
+		message.textContent = "Welcome, " + user.displayName;
+		document.body.classList.add('auth')
+	} else {
+		document.body.classList.remove('auth');
+		message.textContent = "";
+	}
+});
