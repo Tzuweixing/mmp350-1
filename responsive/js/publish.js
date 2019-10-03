0636e756748b172e3cb67e06f0f9d178edacd724
Owen Roberts <oroberts@bmcc.cuny.edu>
2019-10-02 21:12:14 -0400

    fm

---
+++ responsive/js/publish.js
@@ -0,0 +1,27 @@
+window.addEventListener('load', function() {
+	const publishButton = document.getElementById('publish');
+	const postText = document.getElementById('post-text');
+
+	publishButton.addEventListener('click', publishPost);
+
+	function publishPost() {
+
+		const dbRef = firebase.database().ref('posts');
+		const user = firebase.auth().currentUser;
+
+		const post = {
+			text: postText.value,
+			date: Date.now(),
+			author: user.displayName,
+			id: user.uid
+		};
+		
+		dbRef.push(post)
+			.then(function(success) {
+				console.log(success);
+			})
+			.catch(function(error) {
+				console.log(error.message);
+			});
+	}
+});
