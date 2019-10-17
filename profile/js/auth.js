cf74f57c92efcf09e713ed25104856a9f1204ba0
Owen Roberts <oroberts@bmcc.cuny.edu>
2019-10-16 20:28:06 -0400

    adds profile page

--- profile/js/auth.js
+++ profile/js/auth.js
@@ -17,7 +17,19 @@ firebase.auth().onAuthStateChanged(function(user) {
 	
 	if (user) {
 		document.body.classList.add('auth');
-		displayName.textContent = "Welcome, " + user.displayName;
+
+		/* find user in database */
+		const userRef = firebase.database().ref('users').child(user.uid);
+		userRef.on('value', function(snapshot) {
+			const userInfo = snapshot.val();
+			displayName.textContent = "Welcome, " + userInfo.displayName;
+		});
+		
+		const profileButton = document.getElementById("edit-profile");
+		profileButton.onclick = function() {
+			location.href = "profile.html?uid=" + user.uid;	
+		};
+				
 	} else {
 		document.body.classList.remove('auth');
 		displayName.textContent = "";
