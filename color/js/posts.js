d7bd3b319b6aa07da37498bba48e0ed190ba051f
Owen Roberts <obroberts@gmail.com>
2019-10-28 12:47:56 -0400

    done with publish/post

--- color/js/posts.js
+++ color/js/posts.js
@@ -1,40 +1,48 @@
-const postsDiv = document.getElementById('posts');
+const posts = document.getElementById('posts');
 const postRef = firebase.database().ref('posts');
 
-postRef.on('child_added', function(snapshot) {
-	createPost(snapshot.val());
+function loadPosts() {
+	postRef.on('child_added', function(snapshot) {
+		createPost(snapshot.val());
+	});
+}
+
+function createDiv(_class, text) {
+	const div = document.createElement('div');
+	div.classList.add(_class);
+	div.textContent = text;
+	return div;
+}
+
+function createPost(data) {
+	const post = createDiv('post');
+	const text = createDiv('text', data.text);
+	const info = createDiv('info');
+	const author = createDiv('author', "by " + users[data.uid]);
+
+	const d = new Date(data.date);
+	const date = createDiv('date', "on " + (d.getMonth() + 1) + '.' + d.getDate() + '.' + d.getFullYear());
+
+	post.appendChild(text);
+	post.appendChild(info);
+	info.appendChild(author);
+	info.appendChild(date);
+
+	/* reverse chronological order */
+	posts.insertBefore(post, posts.firstElementChild); 
+}
+
+/* get user data then load posts */
+let userCount = 0;
+const users = {};
+firebase.database().ref('users').on('child_added', function(snapshot) {
+	const user = snapshot.val();
+	users[snapshot.key] = user.displayName;
+	userCount++;
 });
 
-function createPost(post) {
-	const postDiv = document.createElement('div');
-	postDiv.classList.add('post');
-	const postText = document.createElement('div');
-	postText.classList.add('post-text');
-	const postInfo = document.createElement('div');
-	postInfo.classList.add('post-info');
-	
-	const postAuthor = document.createElement('span');
-	postAuthor.classList.add('post-author');
-	const postDate = document.createElement('span');
-	postDate.classList.add('post-date');
-
-	postText.textContent = post.text;
-	postAuthor.textContent = post.author;
-
-	console.log(post.date);
-	var date = new Date(post.date);
-	postDate.textContent = (1+date.getMonth()) + '/' + date.getDate() +  '/' + date.getFullYear();
-
-	postDiv.appendChild(postText);
-	postDiv.appendChild(postInfo);
-	postInfo.innerHTML += "by ";
-	postInfo.appendChild(postAuthor);
-	postInfo.innerHTML += " on ";
-	postInfo.appendChild(postDate);
-
-	
-	/* reverse chronological order */
-	postsDiv.insertBefore(postDiv, postsDiv.firstElementChild); 
-
-	// postsDiv.appendChild(postDiv);  /* for chronological order */
-}
+firebase.database().ref('users').once('value', function(snapshot) {
+	if (snapshot.numChildren() === userCount) {
+		loadPosts();
+	}
+});
